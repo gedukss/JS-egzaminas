74a0c0713da73073936da4e36372b77ab36c785f
gedukss <gedukss@gmail.com>
2022-12-08 19:22:26 +0200

    update

--- Task 5/script.js
+++ Task 5/script.js
@@ -19,3 +19,17 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
   { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
 ];
+
+
+function filterDogOwners(array)  {
+  return array.filter(user => user.hasDog);
+}
+
+console.log(filterDogOwners(users));
+
+
+function filterAdults(array) {
+  return array.filter(user => user.age >= 18);
+}
+
+console.log(filterAdults(users));
